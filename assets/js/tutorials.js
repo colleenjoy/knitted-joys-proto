e84d51f6e9eb6ec4b6405a3f11899664481bcef3
Colleen Higgins <colleenjoyhiggins@gmail.com>
2018-04-11 16:32:28 -0400

    completed the search and drop menu features

--- assets/js/tutorials.js
+++ assets/js/tutorials.js
@@ -1,18 +1,42 @@
 // tutorials page
 
-// click drop menu behind the icon
+// make an array of all the tutorials
 
-function filter() {
-	var tutorialsList = Array.from(document.querySelectorAll('.image-grid a'));
-	console.log(tutorialsList);
+var tutorialsList = Array.from(document.querySelectorAll('.image-grid a'));
+
+// filter based on the drop menu
+function filter(event) {
 	var filteredList = tutorialsList.filter(function(tutorial) {
-		return tutorial.dataset.category === "decorative-stitches";
+		// checking if the drop menu option matches any of the 
+		// data categories given to any tutorials
+		return (tutorial.dataset.category.indexOf(event.target.value) > -1);
 	})
-	console.log(filteredList);
+
+	// display the filtered tutorials by deleting all of them
+	// and then adding them back
 	var imageGrid = document.querySelector('.image-grid');
 	imageGrid.innerHTML = '';
 	filteredList.forEach (function(tutorial) {
 		imageGrid.appendChild(tutorial);	
 	})
 	
+}
+
+// filter based on search entry
+function search(q) {
+	//make search term lower case
+	var searchTerm = document.getElementById('tutorial-search').value.toLowerCase();
+	var filteredList = tutorialsList.filter( function(tutorial) {
+		tutorialLowerCase = tutorial.innerText.toLowerCase();
+		// check for a match
+		return (tutorialLowerCase.indexOf(searchTerm) > -1);
+	})
+	// display the tutorials that matched the search
+	var imageGrid = document.querySelector('.image-grid');
+	imageGrid.innerHTML = '';
+	filteredList.forEach (function(tutorial) {
+		imageGrid.appendChild(tutorial);	
+	})
+	// reset the search bar
+	document.getElementById('tutorial-search').value = '';
 }
