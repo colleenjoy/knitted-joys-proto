fef745cb6cd65ea77d8fa9cbacaee82e1e8e7a40
Colleen Higgins <colleenjoyhiggins@gmail.com>
2018-04-11 11:40:06 -0400

    finished shop all page

--- assets/js/tutorials.js
+++ assets/js/tutorials.js
@@ -3,11 +3,16 @@
 // click drop menu behind the icon
 
 function filter() {
-	var tutorialsList = Array.from(document.querySelectorAll('.image-row a'));
+	var tutorialsList = Array.from(document.querySelectorAll('.image-grid a'));
 	console.log(tutorialsList);
 	var filteredList = tutorialsList.filter(function(tutorial) {
 		return tutorial.dataset.category === "decorative-stitches";
 	})
 	console.log(filteredList);
+	var imageGrid = document.querySelector('.image-grid');
+	imageGrid.innerHTML = '';
+	filteredList.forEach (function(tutorial) {
+		imageGrid.appendChild(tutorial);	
+	})
 	
 }
